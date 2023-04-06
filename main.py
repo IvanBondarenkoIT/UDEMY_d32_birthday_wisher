d0c7c8a5c3a8454ef47971f9eab12aeaad0f4f2f
Ivan <ivan.bondarenko.it@gmail.com>
2023-04-06 19:56:03 +0400

    config mail

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import smtplib
 import config
 
 
-connection = smtplib.SMTP("smtp.gmail.com")
+# connection = smtplib.SMTP("smtp.gmail.com")
+connection = smtplib.SMTP("smtp.meta.ua:25")
 connection.starttls()
 connection.login(user=config.my_email, password=config.password)
 connection.sendmail(from_addr=config.my_email, to_addrs=config.to_email, msg="Hello")
